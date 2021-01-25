dec0681a5560f7b60ff010593cee199a0d8dbbd6
Esemel <34270023+Esemel@users.noreply.github.com>
2021-01-25 11:44:16 +0100

    Version 1.1

---
+++ Assets/Scripts/HintSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HintSceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait_for_intro());
+    }
+
+    IEnumerator Wait_for_intro()
+    {
+        yield return new WaitForSeconds(8.0f);
+
+        SceneManager.LoadScene("Level1");
+
+    }
+}
